96457c4ed0015e5e1fd48deb58d189938f982bef
Clarice Bouwer <clarice.bouwer@dstvdm.com>
2018-04-24 06:14:47 +0200

    Show post navigation on all pages

--- src/layouts/PaginatedContent/PaginatedContent.jsx
+++ src/layouts/PaginatedContent/PaginatedContent.jsx
@@ -6,20 +6,15 @@ import "./PaginatedContent.css";
 class PaginatedContent extends React.Component {
   render() {
     const { page, pages, prev, next, children } = this.props;
-    let className = "";
-    if (page > 1) {
-      className = `${className} paged`;
-    }
+    let className = "paged";
     return (
       <MainContent className={className}>
-        {/* Previous/next page links - only displayed on page 2+ */}
         <div className="extra-pagination inner">
           <Pagination page={page} pages={pages} prev={prev} next={next} />
         </div>
 
         {children}
 
-        {/* Previous/next page links - displayed on every page */}
         <Pagination page={page} pages={pages} prev={prev} next={next} />
       </MainContent>
     );
