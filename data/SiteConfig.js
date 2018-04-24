97b2a5cb2f73362bf269a8d8ad41b37f3270b53a
Clarice Bouwer <clarice.bouwer@dstvdm.com>
2018-04-24 20:34:04 +0200

    Remove comments from SiteConfig

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -9,7 +9,7 @@ module.exports = {
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
   siteDescription: "Journal entries for self-discovery and corporate world experience.",
   siteCover: "https://picsum.photos/1920/639/?image=983",
-  siteNavigation: true, // If navigation is enabled the Menu button will be visible
+  siteNavigation: true,
   siteRss: "/feed.xml",
   siteRssAuthor: "Clarice Bouwer",
   siteFBAppID: "1685950495005107",
@@ -21,8 +21,7 @@ module.exports = {
     { url: "https://twitter.com/cbillowes", type: "twitter" },
     { url: "mailto:hello@corporateprogrammer.com", type: "email" }
   ],
-  postDefaultCategoryID: "Tech", // Default category for posts.
-  // Links to social profiles/projects you want to display in the navigation bar.
+  postDefaultCategoryID: "Tech",
   userLinks: [
     {
       label: "GitHub",
@@ -45,5 +44,5 @@ module.exports = {
   },
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
   backgroundColor: "#e0e0e0", // Used for setting manifest background color.
-  promoteGatsby: true // Enables the GatsbyJS promotion information in footer.
+  promoteGatsby: true
 };
